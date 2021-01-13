9c4e591a81ff847acf5215ae922db17b24e3c461
michalwojewoda <mw.pryw@gmail.com>
2021-01-13 01:28:32 +0100

    simple madlibs game

---
+++ games/madlibs/madlibs.py
@@ -0,0 +1,12 @@
+#strings concatoneation
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Another verb: ")
+famous_person = input("Famous person: ")
+
+
+madlib = f"Computer programing is so {adj}! it makes me so excited all the time because \n i love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+print(madlib)
+
